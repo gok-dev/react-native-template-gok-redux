8fc788da7c9e27412ca450504d29a394d7864ddb
Marcos Borges <marcos.abb52@gmail.com>
2021-03-12 15:03:20 -0300

    chore(template): add styled-components and theme

--- template/src/pages/Main/index.tsx
+++ template/src/pages/Main/index.tsx
@@ -1,15 +1,10 @@
 import React, { useEffect } from 'react';
-import {
-  Text,
-  Image,
-  StyleSheet,
-  Dimensions,
-  View,
-  StatusBar
-} from 'react-native';
+import { StatusBar } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { MainActions } from 'store/ducks/main'
+
+import { Container, Logo, Welcome, File, Instructions } from './styles'
 
 export default function Main() {
   const dispatch = useDispatch()
@@ -20,62 +15,19 @@ export default function Main() {
   }, []);
 
   return (
-    <View style={styles.container}>
+    <Container>
       <StatusBar barStyle="dark-content" />
-      <Image
+      <Logo
         source={{
           uri: 'https://s3-sa-east-1.amazonaws.com/prod-jobsite-files.kenoby.com/uploads/goKdigital-1570109201-gopng.png',
         }}
-        style={styles.logo}
         resizeMode="contain"
       />
-      <Text style={styles.welcome}>{title}</Text>
-      <Text style={styles.instructions}>Essa é a tela principal da sua aplicação =)</Text>
-      <Text style={styles.instructions}>Você pode editar a tela no arquivo:</Text>
-      <Text style={[styles.instructions, styles.fileName]}>src/pages/Main/index.tsx</Text>
-    </View>
+      <Welcome>{title}</Welcome>
+      <Instructions>Essa é a tela principal da sua aplicação =)</Instructions>
+      <Instructions>Você pode editar a tela no arquivo:</Instructions>
+      <File>src/pages/Main/index.tsx</File>
+    </Container>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    flex: 1,
-    paddingHorizontal: 20,
-    backgroundColor: '#FFF'
-  },
-  fileName: {
-    fontWeight: 'bold',
-    marginTop: 5,
-  },
-  instructions: {
-    color: '#333',
-    fontSize: 14,
-    marginTop: 20,
-    textAlign: 'center',
-  },
-  logo: {
-    height: Dimensions.get('window').height * 0.11,
-    marginVertical: Dimensions.get('window').height * 0.11,
-    width: Dimensions.get('window').height * 0.11 * (1950 / 662),
-  },
-  welcome: {
-    color: '#50cc1a',
-    fontSize: 22,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  button: {
-    width: '100%',
-    height: 50,
-    backgroundColor: '#50cc1a',
-    borderRadius: 5,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  buttonText: {
-    fontSize: 18,
-    color: '#FFF',
-    fontWeight: 'bold'
-  }
-});
---
+++ template/src/pages/Main/styles.ts
@@ -0,0 +1,37 @@
+import styled from 'styled-components/native';
+import { theme } from 'styled-tools'
+import { Dimensions } from 'react-native'
+
+const { height } = Dimensions.get('window')
+
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  padding-horizontal: 20px;
+  background-color: ${theme('colors.background')};
+`;
+
+export const Logo = styled.Image`
+  width: ${height * 0.11 * (1950 / 662)}px;
+  height: ${height * 0.11}px;
+  margin-vertical: ${height * 0.11}px;
+`
+
+export const Welcome = styled.Text`
+  font-size: 22px;
+  font-weight: bold;
+  text-align: center;
+  color: ${theme('colors.primary')};
+`
+
+export const Instructions = styled.Text`
+  margin-top: 20px;
+  font-size: 14px;
+  text-align: center;
+  color: ${theme('colors.text')};
+`;
+
+export const File = styled(Instructions)`
+  margin-top: 5px;
+  font-weight: bold;
+`
